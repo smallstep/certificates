0d4f96c1d9a9d78fc5a9cdc727e83723930803af
Mariano Cano <mariano@smallstep.com>
2018-11-20 16:47:08 -0800

    Update renew with force flag.
    Clean old certificates in volumes on start.

--- examples/docker/renewer/entrypoint.sh
+++ examples/docker/renewer/entrypoint.sh
@@ -3,16 +3,16 @@
 # Wait for CA
 sleep 5
 
-if [ ! -f /var/local/step/root_ca.crt ]; then
-    # Donwload the root certificate
-    step ca root /var/local/step/root_ca.crt
-fi
+# Clean old certificates
+rm -f /var/local/step/root_ca.crt
+rm -f /var/local/step/site.crt /var/local/step/site.key
 
-if [ ! -f /var/local/step/site.crt ]; then
-    # Get token
-    STEP_TOKEN=$(step ca token $COMMON_NAME)
-    # Donwload the root certificate
-    step ca certificate --token $STEP_TOKEN $COMMON_NAME /var/local/step/site.crt /var/local/step/site.key
-fi
+# Donwload the root certificate
+step ca root /var/local/step/root_ca.crt
+
+# Get token
+STEP_TOKEN=$(step ca token $COMMON_NAME)
+# Donwload the root certificate
+step ca certificate --token $STEP_TOKEN $COMMON_NAME /var/local/step/site.crt /var/local/step/site.key
 
 exec "$@"
