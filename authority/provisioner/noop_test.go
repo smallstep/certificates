5dfcbcf5dcf382a15286c7383a3933697a7c7767
Mariano Cano <mariano@smallstep.com>
2019-03-11 12:56:47 -0700

    Add noop tests.

---
+++ authority/provisioner/noop_test.go
@@ -0,0 +1,27 @@
+package provisioner
+
+import (
+	"crypto/x509"
+	"testing"
+
+	"github.com/smallstep/assert"
+)
+
+func Test_noop(t *testing.T) {
+	p := noop{}
+	assert.Equals(t, "noop", p.GetID())
+	assert.Equals(t, "noop", p.GetName())
+	assert.Equals(t, noopType, p.GetType())
+	assert.Equals(t, nil, p.Init(Config{}))
+	assert.Equals(t, nil, p.AuthorizeRenewal(&x509.Certificate{}))
+	assert.Equals(t, nil, p.AuthorizeRevoke("foo"))
+
+	kid, key, ok := p.GetEncryptedKey()
+	assert.Equals(t, "", kid)
+	assert.Equals(t, "", key)
+	assert.Equals(t, false, ok)
+
+	sigOptions, err := p.Authorize("foo")
+	assert.Equals(t, []SignOption{}, sigOptions)
+	assert.Equals(t, nil, err)
+}
