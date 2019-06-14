e852b57870d627c1258990e2fe69eb9ee6224f23
Mariano Cano <mariano@smallstep.com>
2019-06-14 16:23:50 -0700

    Delete old autocert

--- autocert/examples/hello-mtls/envoy/certwatch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true; do
-    inotifywait -e modify /var/run/autocert.step.sm/site.crt
-    kill -HUP 1
-done
--- autocert/examples/hello-mtls/nginx/certwatch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true; do
-    inotifywait -e modify /var/run/autocert.step.sm/site.crt
-    nginx -s reload
-done
