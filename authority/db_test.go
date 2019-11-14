efc2180c4a65b7a45c9d6ebe8072461c3e88bc24
Mariano Cano <mariano@smallstep.com>
2019-11-14 10:49:13 -0800

    Complete AuthDB interface.

--- authority/db_test.go
+++ authority/db_test.go
@@ -8,31 +8,30 @@ import (
 )
 
 type MockAuthDB struct {
-	err                 error
-	ret1                interface{}
-	init                func(*db.Config) (db.AuthDB, error)
-	isRevoked           func(string) (bool, error)
-	revoke              func(rci *db.RevokedCertificateInfo) error
-	storeCertificate    func(crt *x509.Certificate) error
-	useToken            func(id, tok string) (bool, error)
-	isSSHHost           func(principal string) (bool, error)
-	storeSSHCertificate func(crt *ssh.Certificate) error
-	shutdown            func() error
-}
-
-func (m *MockAuthDB) Init(c *db.Config) (db.AuthDB, error) {
-	if m.init != nil {
-		return m.init(c)
-	}
-	if m.ret1 == nil {
-		return nil, m.err
-	}
-	return m.ret1.(*db.DB), m.err
+	err                  error
+	ret1                 interface{}
+	isRevoked            func(string) (bool, error)
+	isSSHRevoked         func(string) (bool, error)
+	revoke               func(rci *db.RevokedCertificateInfo) error
+	revokeSSH            func(rci *db.RevokedCertificateInfo) error
+	storeCertificate     func(crt *x509.Certificate) error
+	useToken             func(id, tok string) (bool, error)
+	isSSHHost            func(principal string) (bool, error)
+	storeSSHCertificate  func(crt *ssh.Certificate) error
+	getSSHHostPrincipals func() ([]string, error)
+	shutdown             func() error
 }
 
 func (m *MockAuthDB) IsRevoked(sn string) (bool, error) {
 	if m.isRevoked != nil {
 		return m.isRevoked(sn)
+	}
+	return m.ret1.(bool), m.err
+}
+
+func (m *MockAuthDB) IsSSHRevoked(sn string) (bool, error) {
+	if m.isSSHRevoked != nil {
+		return m.isSSHRevoked(sn)
 	}
 	return m.ret1.(bool), m.err
 }
@@ -50,6 +49,13 @@ func (m *MockAuthDB) UseToken(id, tok string) (bool, error) {
 func (m *MockAuthDB) Revoke(rci *db.RevokedCertificateInfo) error {
 	if m.revoke != nil {
 		return m.revoke(rci)
+	}
+	return m.err
+}
+
+func (m *MockAuthDB) RevokeSSH(rci *db.RevokedCertificateInfo) error {
+	if m.revokeSSH != nil {
+		return m.revokeSSH(rci)
 	}
 	return m.err
 }
@@ -73,6 +79,13 @@ func (m *MockAuthDB) StoreSSHCertificate(crt *ssh.Certificate) error {
 		return m.storeSSHCertificate(crt)
 	}
 	return m.err
+}
+
+func (m *MockAuthDB) GetSSHHostPrincipals() ([]string, error) {
+	if m.getSSHHostPrincipals != nil {
+		return m.getSSHHostPrincipals()
+	}
+	return m.ret1.([]string), m.err
 }
 
 func (m *MockAuthDB) Shutdown() error {
