8eff4e77a893e16d381427ae60075c223c86d584
Mariano Cano <mariano@smallstep.com>
2020-09-14 19:12:49 -0700

    Comment request structs.

--- cas/apiv1/requests.go
+++ cas/apiv1/requests.go
@@ -5,23 +5,29 @@ import (
 	"time"
 )
 
+// CreateCertificateRequest is the request used to sign a new certificate.
 type CreateCertificateRequest struct {
 	Template  *x509.Certificate
 	Lifetime  time.Duration
 	Backdate  time.Duration
 	RequestID string
 }
+
+// CreateCertificateResponse is the response to a create certificate request.
 type CreateCertificateResponse struct {
 	Certificate      *x509.Certificate
 	CertificateChain []*x509.Certificate
 }
 
+// RenewCertificateRequest is the request used to re-sign a certificate.
 type RenewCertificateRequest struct {
 	Template  *x509.Certificate
 	Lifetime  time.Duration
 	Backdate  time.Duration
 	RequestID string
 }
+
+// RenewCertificateResponse is the response to a renew certificate request.
 type RenewCertificateResponse struct {
 	Certificate      *x509.Certificate
 	CertificateChain []*x509.Certificate
@@ -30,7 +36,12 @@ type RenewCertificateResponse struct {
 // RevokeCertificateRequest is the request used to revoke a certificate.
 type RevokeCertificateRequest struct {
 	Certificate *x509.Certificate
+	Reason      string
+	ReasonCode  int
+	RequestID   string
 }
+
+// RevokeCertificateResponse is the response to a revoke certificate request.
 type RevokeCertificateResponse struct {
 	Certificate      *x509.Certificate
 	CertificateChain []*x509.Certificate
