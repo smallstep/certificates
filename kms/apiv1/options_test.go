d9da150a5fe10882187c9a79b96189261f52a315
Mariano Cano <mariano.cano@gmail.com>
2021-01-27 20:23:45 -0800

    Fix test.

--- kms/apiv1/options_test.go
+++ kms/apiv1/options_test.go
@@ -42,7 +42,7 @@ func TestErrNotImplemented_Error(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := ErrNotImplemented{
-				msg: tt.fields.msg,
+				Message: tt.fields.msg,
 			}
 			if got := e.Error(); got != tt.want {
 				t.Errorf("ErrNotImplemented.Error() = %v, want %v", got, tt.want)
