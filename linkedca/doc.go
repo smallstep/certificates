35cfa5b8a298e2a4d96f0daed16f9417052622b7
Mariano Cano <mariano.cano@gmail.com>
2021-05-24 12:43:23 -0700

    Remove majordomo client and rename administrator to admin.

--- linkedca/doc.go
+++ linkedca/doc.go
@@ -1,3 +1,3 @@
 package majordomo
 
-//go:generate protoc --proto_path=.. --go_out=.. --go-grpc_out=.. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative majordomo/provisioners.proto majordomo/majordomo.proto
+//go:generate protoc --proto_path=.. --go_out=.. --go-grpc_out=.. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative linkedca/provisioners.proto linkedca/admin.proto
