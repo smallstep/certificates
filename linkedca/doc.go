d4a9779ec89655f35615268eda0fa2749b954573
Mariano Cano <mariano.cano@gmail.com>
2021-05-24 12:46:16 -0700

    Fix package name.

--- linkedca/doc.go
+++ linkedca/doc.go
@@ -1,3 +1,3 @@
-package majordomo
+package linkedca
 
 //go:generate protoc --proto_path=.. --go_out=.. --go-grpc_out=.. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative linkedca/provisioners.proto linkedca/admin.proto
