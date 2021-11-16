922d2391715901cafa8ed8e7741b0303ff442087
max furman <mx.furman@gmail.com>
2021-11-16 10:02:04 -0800

    Simplify conditional

--- authority/ssh.go
+++ authority/ssh.go
@@ -105,11 +105,9 @@ func (a *Authority) GetSSHConfig(ctx context.Context, typ string, data map[strin
 		// Backwards compatibility for version of the cli older than v0.18.0.
 		// Before v0.18.0 we were not passing any value for SSHTemplateVersionKey
 		// from the cli.
-		if o.Name == "step_includes.tpl" {
-			if val, ok := data[templates.SSHTemplateVersionKey]; !ok || val == "" {
-				o.Type = templates.File
-				o.Path = strings.TrimPrefix(o.Path, "${STEPPATH}/")
-			}
+		if o.Name == "step_includes.tpl" && data[templates.SSHTemplateVersionKey] == "" {
+			o.Type = templates.File
+			o.Path = strings.TrimPrefix(o.Path, "${STEPPATH}/")
 		}
 
 		output = append(output, o)
