8ec984575251c4bd628485180d1e9ca9009615d9
Panagiotis Siatras <azazeal@users.noreply.github.com>
2022-03-28 17:02:19 +0300

    api/render: added test coverage for Error

--- api/render/render_test.go
+++ api/render/render_test.go
@@ -3,10 +3,12 @@ package render
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
-	"github.com/smallstep/certificates/logging"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/smallstep/certificates/logging"
 )
 
 func TestJSON(t *testing.T) {
@@ -26,4 +28,63 @@ func TestJSONPanics(t *testing.T) {
 	assert.Panics(t, func() {
 		JSON(httptest.NewRecorder(), make(chan struct{}))
 	})
+}
+
+type renderableTestError struct {
+	Code    int    `json:"-"`
+	Message string `json:"message"`
+}
+
+func (err renderableTestError) Error() string {
+	return err.Message
+}
+
+func (err renderableTestError) Render(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "something/custom")
+
+	JSONStatus(w, err, err.Code)
+}
+
+type codedTestError struct {
+	Contents string
+}
+
+func (err codedTestError) Error() string { return err.Contents }
+
+func (codedTestError) StatusCode() int { return 432 }
+
+func TestError(t *testing.T) {
+	cases := []struct {
+		err    error
+		code   int
+		body   string
+		header string
+	}{
+		0: {
+			err:    renderableTestError{532, "some string"},
+			code:   532,
+			body:   "{\"message\":\"some string\"}\n",
+			header: "something/custom",
+		},
+		1: {
+			err:    codedTestError{"123"},
+			code:   432,
+			body:   "{\"Contents\":\"123\"}\n",
+			header: "application/json",
+		},
+	}
+
+	for caseIndex := range cases {
+		kase := cases[caseIndex]
+
+		t.Run(strconv.Itoa(caseIndex), func(t *testing.T) {
+			rec := httptest.NewRecorder()
+
+			Error(rec, kase.err)
+
+			assert.Equal(t, kase.code, rec.Result().StatusCode)
+			assert.Equal(t, kase.body, rec.Body.String())
+			assert.Equal(t, kase.header, rec.Header().Get("Content-Type"))
+		})
+	}
 }
