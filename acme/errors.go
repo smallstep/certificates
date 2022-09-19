59211eee405e8ebde0b29c9b6d8bf0741c40d8dd
Mariano Cano <mariano.cano@gmail.com>
2022-09-19 12:48:35 -0700

    Return the internal error instead of the ACME error
    For ACME errors, return the internal error string instead of the
    ACME one on the "Error() string" function. This way the logs will
    have more information about the cause of an error.
    
    Fixes #1057

--- acme/errors.go
+++ acme/errors.go
@@ -338,7 +338,10 @@ func (e *Error) StatusCode() int {
 
 // Error allows AError to implement the error interface.
 func (e *Error) Error() string {
-	return e.Detail
+	if e.Err == nil {
+		return e.Detail
+	}
+	return e.Err.Error()
 }
 
 // Cause returns the internal error and implements the Causer interface.
