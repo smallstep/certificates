e6dd2116372be315cf37ab1dd143a9ee249a369d
Stefan Berthold <stefan.berthold@wire.com>
2023-02-02 11:14:29 +0100

    acquire DPoP signing key from provisioner

--- authority/provisioner/dpop_options.go
+++ authority/provisioner/dpop_options.go
@@ -4,6 +4,8 @@ type DPOPOptions struct {
 	// ValidationExecPath is the name of the executable to call for DPOP
 	// validation.
 	ValidationExecPath string `json:"validation-exec-path,omitempty"`
+	// Backend signing key for DPoP access token
+	SigningKey string `json:"key"`
 }
 
 func (o *DPOPOptions) GetValidationExecPath() string {
@@ -11,4 +13,11 @@ func (o *DPOPOptions) GetValidationExecPath() string {
 		return "rusty-jwt-cli"
 	}
 	return o.ValidationExecPath
+}
+
+func (o *DPOPOptions) GetSigningKey() string {
+	if o == nil {
+		return ""
+	}
+	return o.SigningKey
 }
