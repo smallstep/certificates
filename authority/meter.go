49e1170fae5e008f2a45c33f05c79bfbfb539970
Panagiotis Siatras <azazeal@users.noreply.github.com>
2024-01-22 22:35:50 +0200

    fixed comments

--- authority/meter.go
+++ authority/meter.go
@@ -2,34 +2,34 @@ package authority
 
 // Meter wraps the set of defined callbacks for metrics gatherers.
 type Meter interface {
-	// X509Signed is called whenever a X509 CSR is signed.
+	// X509Signed is called whenever an X509 certificate is signed.
 	X509Signed(provisioner string, success bool)
 
-	// X509Renewed is called whenever a X509 certificate is renewed.
+	// X509Renewed is called whenever an X509 certificate is renewed.
 	X509Renewed(provisioner string, success bool)
 
-	// X509Rekeyed is called whenever a X509 certificate is rekeyed.
+	// X509Rekeyed is called whenever an X509 certificate is rekeyed.
 	X509Rekeyed(provisioner string, success bool)
 
-	// X509Authorized is called whenever a X509 authoring webhook is called.
+	// X509Authorized is called whenever an X509 authoring webhook is called.
 	X509Authorized(provisioner string, success bool)
 
-	// X509Enriched is called whenever a X509 enriching webhook is called.
+	// X509Enriched is called whenever an X509 enriching webhook is called.
 	X509Enriched(provisioner string, success bool)
 
-	// SSHSigned is called whenever a SSH CSR is signed.
+	// SSHSigned is called whenever an SSH certificate is signed.
 	SSHSigned(provisioner string, success bool)
 
-	// SSHRenewed is called whenever a SSH certificate is renewed.
+	// SSHRenewed is called whenever an SSH certificate is renewed.
 	SSHRenewed(provisioner string, success bool)
 
-	// SSHRekeyed is called whenever a SSH certificate is rekeyed.
+	// SSHRekeyed is called whenever an SSH certificate is rekeyed.
 	SSHRekeyed(provisioner string, success bool)
 
-	// SSHAuthorized is called whenever a SSH authoring webhook is called.
+	// SSHAuthorized is called whenever an SSH authoring webhook is called.
 	SSHAuthorized(provisioner string, success bool)
 
-	// SSHEnriched is called whenever a SSH enriching webhook is called.
+	// SSHEnriched is called whenever an SSH enriching webhook is called.
 	SSHEnriched(provisioner string, success bool)
 
 	// KMSSigned is called per KMS signer signature.
