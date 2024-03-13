976bf0c2a2210ad53734c34d9c2005ad10dc1450
Mariano Cano <mariano.cano@gmail.com>
2024-03-12 18:26:57 -0700

    Do not fail if a provisioner cannot be initialized
    This commit will mark a provisioner as disabled if it fails to
    initialize. The provisioner will be visible, but authorizing a token
    with a disabled provisioner will always fail.
    
    Fixes: #589, #1757

--- authority/authority.go
+++ authority/authority.go
@@ -255,7 +255,10 @@ func (a *Authority) ReloadAdminResources(ctx context.Context) error {
 	provClxn := provisioner.NewCollection(provisionerConfig.Audiences)
 	for _, p := range provList {
 		if err := p.Init(provisionerConfig); err != nil {
-			return err
+			log.Printf("failed to initialize %s provisioner %q: %v\n", p.GetType(), p.GetName(), err)
+			p = provisioner.Disabled{
+				Interface: p, Reason: err,
+			}
 		}
 		if err := provClxn.Store(p); err != nil {
 			return err
