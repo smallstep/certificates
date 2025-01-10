a3db8de6614ea02de4a853a243a571434a232f43
Eric Norris <enorris@etsy.com>
2025-01-10 13:47:19 -0500

    feat(gcp): enable organization checking
    Before this commit, users could specify a hardcoded list of project IDs
    to restrict access to the GCP provisioner. While this works, it can be
    both toilsome to the team maintaining the Smallstep installation and
    unintuitive to the internal infrastructure users that may encounter
    errors as a result of their project not being added.
    
    This commit is a rough attempt at adding support for validating that a
    GCP project belongs to a given GCP organization. It does this by using
    the `projects.getAncestry` call in the Cloud Resource Manager API. If
    a token's project claim does not have the given organization ID as its
    topmost ancestor, the token is rejected. This will require the
    `resourcemanager.projects.get` IAM permission on the organization.
    
    The new `OrganizationID` configuration directive is compatible with the
    existing `ProjectIDs` configuration. If `ProjectIDs` is non-empty, it
    will take precedence over the `OrganizationID` and act as it did before,
    with the minor difference that if `OrganizationID` is also non-empty,
    the provisioner will check the project's ancestry before rejecting the
    token.
    
    There are a couple outstanding questions and tasks after this commit. I
    tried to strike the right balance between production-ready and
    proof-of-concept here, so I'm open to any suggestions.
    
    - Is the `authority/provisioner/gcp` package the right place for adding
      this functionality? Is the new struct the right approach?
    - We should add tests for validating the organization ID.
    - How should users configure the authentication for the Cloud Resource
      Manager client? I expect this would be similar to the Cloud KMS
      integration.
    - Does Smallstep Professional run in an environment that will be able to
      authenticate with Google? We would need to either grant permissions to
      a Smallstep-owned Google service account if it's run in GCP, or set up
      something like Google's Workload Identity Federation to handle a K8s,
      AWS, or Azure deployment.

---
+++ authority/provisioner/gcp/projectvalidator.go
@@ -0,0 +1,78 @@
+package gcp
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/smallstep/certificates/errs"
+
+	"google.golang.org/api/cloudresourcemanager/v1"
+)
+
+type ProjectValidator struct {
+	ProjectIDs []string
+}
+
+func (p *ProjectValidator) ValidateProject(_ context.Context, projectID string) error {
+	if len(p.ProjectIDs) == 0 {
+		return nil
+	}
+
+	for _, pi := range p.ProjectIDs {
+		if pi == projectID {
+			return nil
+		}
+	}
+
+	return errs.Unauthorized("gcp.authorizeToken; invalid gcp token - invalid project id")
+}
+
+type OrganizationValidator struct {
+	*ProjectValidator
+
+	OrganizationID  string
+	projectsService *cloudresourcemanager.ProjectsService
+}
+
+func NewOrganizationValidator(ctx context.Context, projectIDs []string, organizationID string) (*OrganizationValidator, error) {
+	crm, err := cloudresourcemanager.NewService(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &OrganizationValidator{
+		&ProjectValidator{projectIDs},
+		organizationID,
+		crm.Projects,
+	}, nil
+}
+
+func (p *OrganizationValidator) ValidateProject(ctx context.Context, projectID string) error {
+	err := p.ProjectValidator.ValidateProject(ctx, projectID)
+
+	if p.OrganizationID == "" {
+		return err
+	}
+
+	ancestry, err := p.projectsService.
+		GetAncestry(projectID, &cloudresourcemanager.GetAncestryRequest{}).
+		Context(ctx).
+		Do()
+
+	if err != nil {
+		return errs.Wrap(http.StatusInternalServerError, err, "gcp.authorizeToken")
+	}
+
+	if len(ancestry.Ancestor) < 1 {
+		return errs.InternalServer("gcp.authorizeToken; getAncestry response malformed")
+	}
+
+	progenitor := ancestry.Ancestor[len(ancestry.Ancestor)-1]
+
+	if progenitor.ResourceId.Type != "organization" || progenitor.ResourceId.Id != p.OrganizationID {
+		return errs.Unauthorized("gcp.authorizeToken; invalid gcp token - project does not belong to organization")
+	}
+
+	return nil
+}
