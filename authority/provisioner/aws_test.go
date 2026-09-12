b49b78813f031922e96592460bd4304802a2b1f0
Alex Lin <private.linalex@protonmail.com>
2026-09-11 18:03:19 -0700

    Adding additional 5 missing AWS regions for completeness

--- authority/provisioner/aws_test.go
+++ authority/provisioner/aws_test.go
@@ -896,5 +896,5 @@ func TestAWS_HardcodedCertificates(t *testing.T) {
 		assert.True(t, cert.NotAfter.After(time.Now()))
 		certs = append(certs, cert)
 	}
-	assert.Len(t, 34, certs, "expected 34 certificates in aws_certificates.pem, but got %d", len(certs))
+	assert.Len(t, 39, certs, "expected 39 certificates in aws_certificates.pem, but got %d", len(certs))
 }
